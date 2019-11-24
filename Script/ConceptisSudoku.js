54f1aafa57ab79693a2d753bd64be28888ccc489
eHpo <eHpo1@users.noreply.github.com>
2019-11-24 19:50:23 +0800

    美化

--- Script/ConceptisSudoku.js
+++ Script/ConceptisSudoku.js
@@ -1,3 +1,8 @@
+/*
+[Script]
+http-response ^https?:\/\/account\.wps\.cn\/api\/users requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/eHpo1/Surge/master/Script/ConceptisSudoku.js
+*/
+
 var obj = "<key>Free</key>\r\n\t\t\t\t<true />\r\n\t\t\t\t<key>Info</key>";
 
 body = $response.body
